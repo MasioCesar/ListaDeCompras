3e717f54b8c0c6fdd0e292a95fad090e5bb84492
MagirusDeutz <masiomc@gmail.com>
2022-10-25 20:25:14 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  function setFinished(index){
+    setTodos((t) => {
+      t.at(index).alreadyFinished = !t.at(index).alreadyFinished;
+      return [...t]
+    })
+  }
+
+  const addTodo = () => {
+    setTodos((t) => [...t, {name: write, alreadyFinished: false}]);
+  };
+  const [write, setWrite] = useState("");
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Lista de Compras</Text>
+      
+      <TextInput value={write} onChangeText={setWrite} style={styles.textInput}></TextInput>
+      <button onClick={addTodo}>Adicionar Item</button>
+
+      {
+        todos.map((todo, index) => (
+          console.log(todo),
+          <Text key={index}>
+             <input
+              type="checkbox" 
+              id={index}
+              name={todo.name}
+              checked = {todo.alreadyFinished}
+              onChange={() => setFinished(index)}
+              />
+            {todo.name}
+            </Text>
+        ))
+      }
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +54,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    border: "1px solid black",
+    margin: 10,
+  }
 });
